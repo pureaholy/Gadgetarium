15739400bb7b83771647790f11375f6c9c8f213e
Aidai04 <izumisoft1@mail.ru>
2023-03-24 15:45:42 +0300

    add mapping

---
+++ src/main/java/us/peaksoft/gadgetarium/dto/ProductResponse.java
@@ -0,0 +1,13 @@
+package us.peaksoft.gadgetarium.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+
+}
