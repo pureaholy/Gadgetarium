21491902ccce35b03cda37923033461392dfa2a3
Aidai04 <izumisoft1@mail.ru>
2023-03-24 16:36:42 +0300

    fill Controller class with endpoints

--- src/main/java/us/peaksoft/gadgetarium/controller/ProductController.java
+++ src/main/java/us/peaksoft/gadgetarium/controller/ProductController.java
@@ -1,4 +1,42 @@
 package us.peaksoft.gadgetarium.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import us.peaksoft.gadgetarium.dto.ProductRequest;
+import us.peaksoft.gadgetarium.dto.ProductResponse;
+import us.peaksoft.gadgetarium.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/gadgetarium/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductResponse>AllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public ProductResponse save(@RequestBody ProductRequest productRequest){
+       return productService.save(productRequest);
+    }
+
+    @GetMapping("{id}")
+    public ProductResponse getById (@PathVariable("id") Long id){
+        return productService.getById(id);
+    }
+
+    @PutMapping("{id}")
+    public  ProductResponse update(@PathVariable("id") Long id, @RequestBody ProductRequest productRequest){
+        return productService.update(id,productRequest);
+    }
+
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable("id") Long id){
+        productService.delete(id);
+        return "The product with this id: " + " was deleted";
+    }
 }
