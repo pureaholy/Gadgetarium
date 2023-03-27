afc2564f0514687765de35e08fac031632cfe17b
Huawei <toluibaikyzydilnaz@gmail.com>
2023-03-27 19:32:52 +0300

    commit3

--- src/main/java/us/peaksoft/gadgetarium/s3/S3StorageController.java
+++ src/main/java/us/peaksoft/gadgetarium/s3/S3StorageController.java
@@ -1,10 +1,7 @@
 package us.peaksoft.gadgetarium.s3;
-
-import com.amazonaws.services.s3.model.Bucket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
