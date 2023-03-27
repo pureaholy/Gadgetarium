27a6c253b120104dff3bcd0883c51e1bcdc46288
Huawei <toluibaikyzydilnaz@gmail.com>
2023-03-27 15:50:41 +0300

    commit2

--- src/main/java/us/peaksoft/gadgetarium/s3/S3Config.java
+++ src/main/java/us/peaksoft/gadgetarium/s3/S3Config.java
@@ -1,22 +1,29 @@
 package us.peaksoft.gadgetarium.s3;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${s3.access.name}")
-    private  String  accessKey;
-    @Value("${s3.access.secret}")
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
     private String accessSecret;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
-    public AmazonS3Client generateS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey,accessSecret);
-        AmazonS3Client client = new AmazonS3Client(credentials);
-        return client;
+    public AmazonS3 s3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region).build();
     }
 }
--- src/main/java/us/peaksoft/gadgetarium/s3/S3StorageController.java
+++ src/main/java/us/peaksoft/gadgetarium/s3/S3StorageController.java
@@ -3,42 +3,38 @@ package us.peaksoft.gadgetarium.s3;
 import com.amazonaws.services.s3.model.Bucket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class S3StorageController {
-    private final S3Factory s3Factory;
-    @GetMapping(path = "/buckets")
-    public List<Bucket> getAllBucket(){
-        return s3Factory.getAllBuckets();
-    }
-    @PostMapping(path = "/upload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public Map<String , String > uploadFile(@RequestPart(value = "file",required = false)MultipartFile file) throws IOException {
-       s3Factory.uploadFile(file.getOriginalFilename(),file.getBytes());
-       Map<String, String > result = new HashMap<>();
-       result.put("key",file.getOriginalFilename());
-       return result;
+
+    private final S3Service service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/download")
-    public ResponseEntity<ByteArrayResource> uploadFile(@RequestParam(value = "file") String file) throws IOException{
-        byte[] data = s3Factory.getFile(file);
-        ByteArrayResource byteArrayResource = new ByteArrayResource(data);
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
         return ResponseEntity
                 .ok()
                 .contentLength(data.length)
                 .header("Content-type", "application/octet-stream")
-                .header("Content-disposition", "attachment; filename=\"" + file + "\"")
-                .body(byteArrayResource);
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
     }
 }
