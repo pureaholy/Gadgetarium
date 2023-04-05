384e66e20d20980b1653f841eed580dea195f47f
Aidai04 <izumisoft1@mail.ru>
2023-04-05 13:56:12 +0300

    start searching and pagination

--- src/main/java/us/peaksoft/gadgetarium/repository/ProductRepository.java
+++ src/main/java/us/peaksoft/gadgetarium/repository/ProductRepository.java
@@ -16,7 +16,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT COUNT(u) FROM Product u WHERE u.brand=:brand and u.color=:color and u.ram=:ram and u.quantityOfSim=:sim and u.price=:price")
     Long Quantity(@Param("brand") Brand brand, @Param("color") Color color, @Param("ram") String ram, @Param("sim") Long sim, @Param("price") int price);
 
-    @Query("SELECT p from Product p WHERE upper(p.name) LIKE concat('%', :text, '%') or upper(p.brand) LIKE concat('%', :text, '%')")
-    List<ProductResponse> search(@Param("text") String text, int page, int size);
-
 }
