cede6687830dded64fd1e51d8673a2b14d86e864
Aidai04 <izumisoft1@mail.ru>
2023-04-06 17:04:48 +0300

    vica versa

--- src/main/java/us/peaksoft/gadgetarium/service/impl/ProductServiceImpl.java
+++ src/main/java/us/peaksoft/gadgetarium/service/impl/ProductServiceImpl.java
@@ -36,7 +36,7 @@ public class ProductServiceImpl implements ProductService {
         List<Product> products = productRepository.findAll();
         List<ProductResponse> productsList = new ArrayList<>();
         for (Product product : products) {
-            productsList.add(mapToResponse(product));
+            productsList.add(mapToResponseForDescriptionAndSavingPrice(product));
         }
         return productsList;
     }
@@ -56,7 +56,7 @@ public class ProductServiceImpl implements ProductService {
     public ProductResponse save(ProductRequest productRequest) {
         Product product = mapToEntity(productRequest);
         productRepository.save(product);
-        return mapToResponse(product);
+        return mapToResponseForDescriptionAndSavingPrice(product);
     }
 
     @Override
@@ -69,7 +69,7 @@ public class ProductServiceImpl implements ProductService {
             product.setDisPercent(discount.getPercent());
         }
         productRepository.save(product);
-        return mapToResponse(product);
+        return mapToResponseForDescriptionAndSavingPrice(product);
 
     }
 
@@ -80,7 +80,7 @@ public class ProductServiceImpl implements ProductService {
         product.setImage(productRequest.getImage());
         product.setDescription(productRequest.getDescription());
         productRepository.save(product);
-        return mapToResponse(product);
+        return mapToResponseForDescriptionAndSavingPrice(product);
     }
 
     @Override
@@ -110,13 +110,13 @@ public class ProductServiceImpl implements ProductService {
         product.setPDF(productRequest.getPDF());
         product.setDescription(productRequest.getDescription());
         productRepository.saveAndFlush(product);
-        return mapToResponse(product);
+        return mapToResponseForDescriptionAndSavingPrice(product);
     }
 
     @Override
     public ProductResponse getById(Long id) {
         Product product = productRepository.findById(id).get();
-        return mapToResponse(product);
+        return mapToResponseForDescriptionAndSavingPrice(product);
     }
 
     @Override
@@ -176,7 +176,36 @@ public class ProductServiceImpl implements ProductService {
         return product;
     }
 
-        private ProductResponse mapToResponse(Product product) {
+
+    private ProductResponse mapToResponse(Product product) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(product.getId());
+        productResponse.setName(product.getName());
+        productResponse.setPrice(product.getPrice());
+        productResponse.setBrand(product.getBrand());
+        productResponse.setColor(product.getColor());
+        productResponse.setDateOfIssue(product.getDateOfIssue());
+        productResponse.setOs(product.getOs());
+        productResponse.setRam(product.getRam());
+        productResponse.setRom(product.getRom());
+        productResponse.setSim(product.getSim());
+        productResponse.setQuantityOfSim(product.getQuantityOfSim());
+        productResponse.setCpu(product.getCpu());
+        productResponse.setWeight(product.getWeight());
+        productResponse.setGuarantee(product.getGuarantee());
+        productResponse.setImage(product.getImage());
+        productResponse.setDisplayInch(product.getDisplayInch());
+        productResponse.setAppointment(product.getAppointment());
+        productResponse.setCapacityBattery(product.getCapacityBattery());
+        productResponse.setDescription(product.getDescription());
+        productResponse.setPDF(product.getPDF());
+        productResponse.setQuantityOfProducts(productRepository.Quantity(product.getBrand(),
+                product.getColor(), product.getRam(),
+                product.getQuantityOfSim(), product.getPrice()));
+        return productResponse;
+    }
+
+    private ProductResponse mapToResponseForDescriptionAndSavingPrice(Product product) {
         ProductResponse productResponse = new ProductResponse();
         productResponse.setId(product.getId());
         productResponse.setName(product.getName());
