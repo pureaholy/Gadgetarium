f4fce0d84265942149c58d271f78ddacde7be6be
Aidai04 <izumisoft1@mail.ru>
2023-04-08 17:02:40 +0300

    bring out from comments

--- src/main/java/us/peaksoft/gadgetarium/repository/ProductRepository.java
+++ src/main/java/us/peaksoft/gadgetarium/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package us.peaksoft.gadgetarium.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
