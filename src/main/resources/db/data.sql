c199ed03539d3953a2acbed0820cfcf53e2eed5f
Aidai04 <izumisoft1@mail.ru>
2023-04-19 20:22:57 +0300

    change data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,9 +1,6 @@
 INSERT INTO address(city_name, country_name, postal_code, state_name, street_name)
 VALUES ('Moscow', 'Russia', '12321', 'MO', 'Arbatsckaya');
 
-INSERT INTO basket(id)
-VALUES (1);
-
 INSERT INTO chosen(id)
 VALUES (1);
 
@@ -22,19 +19,6 @@ VALUES (1, '2023-04-01', '2023-03-25');
 INSERT INTO categories(id, name, subcat)
 VALUES (1, 'Phone', 'SMARTPHONES');
 
-INSERT INTO products(id, appointment, brand,
-                     capacity_battery, color, price,
-                     cpu, date_of_issue,
-                     display_inch, guarantee,
-                     image, name, os,
-                     ram, rom,
-                     sim, weight,
-                     basket_id, chosen_id,
-                     discount_id,
-                     news_id, order_list_id, promotion_id, category_id)
-VALUES (1, 'phone', 'ASUS', '128GB', 'RED', 45000, 'DWQ2', '01-02-2024',
-        '5.6MPX', 'YES', 'HJF5849JK39JSK', 'IPHONE', 'IOS', '57TU', '567JK', 'TELE2', '450GR', 1, 1, 1, 1, 1, 1, 1);
-
 INSERT INTO payments(id, amount, created_date, payment_method)
 VALUES (1, 53, '01-02-2023', 'CART');
 
@@ -44,6 +28,22 @@ VALUES (1, 'Mamatbekova', 'Aidai', '+78956673387');
 INSERT INTO users(email, first_name, last_name, password, role)
 VALUES ('admin@gmail.com', 'Admin', 'Adminov', '$2a$12$zUO1dmnQw.y5asgrJyiNj.TtPFZ3TWj6i5C0mRXy.Sx8wIJrw51j6', 'ADMIN'),
        ('user@gmail.com', 'User', 'Userov', '$2a$12$GUqbAtoXhNd8qQ1izXUQIu06x8SrLGMXGLv28r1KYsVNyWl/5.rNi', 'USER');
+
+INSERT INTO basket(id,user_id)
+VALUES (1,2);
+
+INSERT INTO products(id, appointment, brand,
+                     capacity_battery, color, price,
+                     cpu, date_of_issue,
+                     display_inch, guarantee,
+                     image, name, os,
+                     ram, rom,
+                     sim, weight,
+                      chosen_id,
+                     discount_id,
+                     news_id, order_list_id, promotion_id, category_id)
+VALUES (1, 'phone', 'ASUS', '128GB', 'RED', 45000, 'DWQ2', '01-02-2024',
+        '5.6MPX', 'YES', 'HJF5849JK39JSK', 'IPHONE', 'IOS', '57TU', '567JK', 'TELE2', '450GR', 1, 1, 1, 1, 1, 1);
 
 INSERT INTO orders(id, count_of_product, order_status,
                    shipping, total_sum,
