c31bd5961ae4e98a555479125856a6cf420d6dac
Aidai04 <izumisoft1@mail.ru>
2023-04-24 18:22:24 +0300

    change names of endpoints and add swagger explanations

--- src/main/java/us/peaksoft/gadgetarium/controller/BasketController.java
+++ src/main/java/us/peaksoft/gadgetarium/controller/BasketController.java
@@ -1,5 +1,7 @@
 package us.peaksoft.gadgetarium.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,32 +13,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import us.peaksoft.gadgetarium.dto.*;
 import us.peaksoft.gadgetarium.service.BasketService;
+
+import java.time.format.DecimalStyle;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/cart")
+@Tag(name = "BasketController", description = "API endpoints for managing products")
 @RequiredArgsConstructor
+
 public class BasketController {
 
     private final BasketService basketService;
 
-    @PostMapping("save-products/{id}")
+    @PostMapping("products/{id}")
+    @Operation(description = "Only users can add a Product to Cart")
     public ProductResponse saveProduct(@PathVariable("id") Long id, @RequestBody ProductRequest productRequest) {
         return basketService.saveProductIntoBasket(id, productRequest);
     }
 
-    @PutMapping("remove-products/{id}")
+    @PutMapping("products/{id}")
+    @Operation(description = "Users and Admin can delete a Product from Cart")
     public SimpleResponse deleteProduct(@PathVariable("id") Long id, @RequestBody ProductRequest productRequest) {
         return basketService.removeProductFromBasket(id, productRequest);
     }
 
 
-    @PutMapping("remove-all-products/{basketId}")
+    @PutMapping("all-products/{basketId}")
+    @Operation(description = "Users and Admin can delete a List of Products from Cart")
     public SimpleResponse deleteAllProduct(@PathVariable("basketId") Long id, @RequestBody ProductRequest productRequest){
         return basketService.removeAllProductFromBasket(id,productRequest);
     }
 
     @GetMapping("products/{id}")
+    @Operation(description = "Users and Admin can see a List of Cart's Products")
     public List<ProductResponse> productsOfCart(@PathVariable(name = "id", required = false) Long id, @RequestParam(value = "page", required = false)
     int page, @RequestParam(name = "size", required = false) int size) {
         return basketService.getProductsByBasketId(id, page, size);
